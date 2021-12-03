8bc4438d5ecf0079cd762daa17b86e024026147b
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2021-12-04 00:38:38 +0100

    chore: use 11ty addExtension config

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,14 @@
+const { parse } = require("csv-parse/sync");
+
 module.exports = function (eleventyConfig) {
+  // https://www.maxkoehler.com/posts/eleventy-csv/
+  eleventyConfig.addDataExtension("csv", (contents) => {
+    const records = parse(contents, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+    return records;
+  });
+
   return eleventyConfig;
 };
